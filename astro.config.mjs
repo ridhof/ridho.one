7485beb30b96c32240fcc7e25b34e47b4bd6e920
Ridho Febriansa <Ridho Febriansa>
2022-08-13 06:43:01 +0700

    fix: config to follow up new dns

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import preact from '@astrojs/preact';
 // https://astro.build/config
 export default defineConfig({
   integrations: [preact()],
-  site: "https://ridhof.github.io",
-  base: "/ridho.one",
+  site: "https://ridho.one",
+  base: "/",
 });
